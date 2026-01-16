1c1fd25cfd442bcfddaa17bf3e69acfe9e9c07dc
Xav519 <dupuis_x@yahoo.com>
2026-01-15 22:35:20 -0500

    Finished the aws_kms_alias section resource

--- modules/secrets-manager/main.tf
+++ modules/secrets-manager/main.tf
@@ -14,3 +14,9 @@ resource "aws_kms_key" "secrets" {
     }
   )
 }
+
+resource "aws_kms_alias" "secrets" {
+  count         = var.create_db_secret || var.create_api_secret || var.create_app_config_secret ? 1 : 0
+  name          = "alias/${var.name_prefix}-secrets"
+  target_key_id = aws_kms_key.secrets[0].key_id
+}

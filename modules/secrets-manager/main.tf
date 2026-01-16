91576ee98a9a976855fbdf1974acb1317a72a617
Xav519 <dupuis_x@yahoo.com>
2026-01-15 22:35:51 -0500

    Added database credentials section in main

--- modules/secrets-manager/main.tf
+++ modules/secrets-manager/main.tf
@@ -20,3 +20,33 @@ resource "aws_kms_alias" "secrets" {
   name          = "alias/${var.name_prefix}-secrets"
   target_key_id = aws_kms_key.secrets[0].key_id
 }
+
+# Secrets Manager - Database Credentials
+resource "aws_secretsmanager_secret" "db_credentials" {
+  count                   = var.create_db_secret ? 1 : 0
+  name                    = "${var.name_prefix}-db-credentials"
+  description             = "Database credentials for ${var.name_prefix}"
+  kms_key_id              = aws_kms_key.secrets[0].id
+  recovery_window_in_days = 7
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.name_prefix}-db-credentials"
+      Type = "database"
+    }
+  )
+}
+
+resource "aws_secretsmanager_secret_version" "db_credentials" {
+  count     = var.create_db_secret ? 1 : 0
+  secret_id = aws_secretsmanager_secret.db_credentials[0].id
+  secret_string = jsonencode({
+    username = var.db_username
+    password = var.db_password
+    engine   = var.db_engine
+    host     = var.db_host
+    port     = var.db_port
+    dbname   = var.db_name
+  })
+}

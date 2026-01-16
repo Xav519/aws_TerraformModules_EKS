39846fd06b0f574c6668f9b8ad24ec2f868a2ca0
Xav519 <dupuis_x@yahoo.com>
2026-01-15 22:36:42 -0500

    Completed the main section

--- modules/secrets-manager/main.tf
+++ modules/secrets-manager/main.tf
@@ -76,3 +76,61 @@ resource "aws_secretsmanager_secret_version" "api_keys" {
     api_secret = var.api_secret
   })
 }
+
+# Secrets Manager - Application Config
+resource "aws_secretsmanager_secret" "app_config" {
+  count                   = var.create_app_config_secret ? 1 : 0
+  name                    = "${var.name_prefix}-app-config"
+  description             = "Application configuration for ${var.name_prefix}"
+  kms_key_id              = aws_kms_key.secrets[0].id
+  recovery_window_in_days = 7
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.name_prefix}-app-config"
+      Type = "application"
+    }
+  )
+}
+
+resource "aws_secretsmanager_secret_version" "app_config" {
+  count         = var.create_app_config_secret ? 1 : 0
+  secret_id     = aws_secretsmanager_secret.app_config[0].id
+  secret_string = jsonencode(var.app_config)
+}
+
+# IAM Policy for reading secrets (only created if at least one secret is enabled)
+resource "aws_iam_policy" "read_secrets" {
+  count       = var.create_db_secret || var.create_api_secret || var.create_app_config_secret ? 1 : 0
+  name_prefix = "${var.name_prefix}-read-secrets-"
+  description = "Allow reading secrets from Secrets Manager"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret"
+        ]
+        Resource = concat(
+          var.create_db_secret ? [aws_secretsmanager_secret.db_credentials[0].arn] : [],
+          var.create_api_secret ? [aws_secretsmanager_secret.api_keys[0].arn] : [],
+          var.create_app_config_secret ? [aws_secretsmanager_secret.app_config[0].arn] : []
+        )
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "kms:Decrypt",
+          "kms:DescribeKey"
+        ]
+        Resource = [aws_kms_key.secrets[0].arn]
+      }
+    ]
+  })
+
+  tags = var.tags
+}
